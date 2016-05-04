0d2d272e4c94debdbff03c05f1498fc3860f291e
fe2s <aka.fe2s@gmail.com>
2016-05-04 14:10:48 +0300

    fix test

--- src/test/scala/io/insightedge/demo/ctr/CtrDemoTest.scala
+++ src/test/scala/io/insightedge/demo/ctr/CtrDemoTest.scala
@@ -1,5 +1,7 @@
 package io.insightedge.demo.ctr
 
+import java.util.Calendar
+
 import io.insightedge.demo.ctr.CtrDemo.DateUtils
 import org.scalatest.FunSuite
 
@@ -10,10 +12,10 @@ class CtrDemoTest extends FunSuite {
 
 
   test("DateUtils parse") {
-    val date@(year, month, day, hour) = DateUtils.parse("14091123")
-
-    assert(date == (2014, 8, 11, 23))
-
+    assert(2014 == DateUtils.parse("14091123", Calendar.YEAR) )
+    assert(8 == DateUtils.parse("14091123", Calendar.MONTH) )
+    assert(11 == DateUtils.parse("14091123", Calendar.DAY_OF_MONTH) )
+    assert(23 == DateUtils.parse("14091123", Calendar.HOUR_OF_DAY) )
   }
 
 }
